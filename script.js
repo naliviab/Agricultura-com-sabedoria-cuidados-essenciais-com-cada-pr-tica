0f4debd3169faf85da357ba8deddf8a808558aa9
kotarox17 <165928025+kotarox17@users.noreply.github.com>
2025-05-23 11:27:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Script básico para interatividade futura
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Site carregado com sucesso!");
+
+    // Aqui você pode adicionar funcionalidades JavaScript, como formulários interativos
+});
 
